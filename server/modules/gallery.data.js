a08c8f94317c3554f66078bf51482db30cc235bb
phaydara <phaydara@gmail.com>
2022-04-29 15:57:43 -0500

    added images to gallery and modified data.js file

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,10 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 1, path: 'images/nature-2.jpg', title: 'Northwoods, Minnesota', description: 'Fall colors on the North Shore of Minnesota.', likes: 0 },
+    { id: 2, path: 'images/travel-3.jpg', title: 'Ulcinj, Montenegro', description: 'Overlook from the Ulcinj Stari Grand hike.', likes: 0 },
+    { id: 3, path: 'images/climbing-2.jpg', title: 'Fontainebleau, France', description: 'Skipping along in our favorite forest.', likes: 0 },
+    { id: 4, path: 'images/food-3.jpg', title: 'Summer in Minneosta', description: 'Grilling in the backyard.', likes: 0 },
+    { id: 5, path: 'images/climbing-3.jpg', title: 'Fontainebleau, France', description: 'Textures in the forest.', likes: 0 },
+    { id: 6, path: 'images/food-5.jpg', title: 'Milly-la-Foret, France', description: 'Rotisserie chicken for dinner.', likes: 0 },
 ];
 
 module.exports = galleryItems;
