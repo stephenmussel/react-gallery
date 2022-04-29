f531e5c715593617aa43200ea7c010387ef64ddc
phaydara <phaydara@gmail.com>
2022-04-29 17:04:14 -0500

    added like button and captured click event

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -1,17 +1,28 @@
 import { useState } from 'react';
+import axios from 'axios';
 
-function GalleryItem({each}) {
+function GalleryItem({ each }) {
 
     const [click, setClick] = useState(true);
-    return(
+
+    const heartPhoto = (photoId) => {
+        console.log('in heartPhoto');
+
+    }
+
+    return (
         <div key={each.id}>
             <div onClick={() => click === true ? setClick(false) : setClick(true)}>
-                {click ? <img src={each.path}/> : <div><h1>{each.description}</h1></div>}
+                {click ? <img src={each.path} /> : <div><p>{each.description}</p></div>}
             </div>
-
-            
-            {/* <h2>{each.title}</h2> */}
-            {/* <p>{each.likes}</p> */}
+            <h2>{each.title}</h2>
+            <p>
+                &#10084;&#65039;
+                &nbsp;&nbsp;
+                {each.likes}
+            </p>
+            <button onClick={() => heartPhoto(each.id)}>Like</button>
+            <br /><br />
         </div>
     )
 }
