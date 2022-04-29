4f00a09b1256965b5b3b89576ce880b03c5ef4a5
phaydara <phaydara@gmail.com>
2022-04-29 16:52:46 -0500

    swapped image with description on click

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -1,10 +1,17 @@
+import { useState } from 'react';
+
 function GalleryItem({each}) {
+
+    const [click, setClick] = useState(true);
     return(
         <div key={each.id}>
-            <img src={each.path} />
-            <h2>{each.title}</h2>
-            <p>{each.description}</p>
-            <p>{each.likes}</p>
+            <div onClick={() => click === true ? setClick(false) : setClick(true)}>
+                {click ? <img src={each.path}/> : <div><h1>{each.description}</h1></div>}
+            </div>
+
+            
+            {/* <h2>{each.title}</h2> */}
+            {/* <p>{each.likes}</p> */}
         </div>
     )
 }
