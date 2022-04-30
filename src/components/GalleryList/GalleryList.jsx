98a8d2738469776c904f84ee037e3744a29c5532
phaydara <phaydara@gmail.com>
2022-04-30 17:45:01 -0500

    added css to gallery list

--- src/components/GalleryList/GalleryList.jsx
+++ src/components/GalleryList/GalleryList.jsx
@@ -1,17 +1,22 @@
 import GalleryItem from "../GalleryItem/GalleryItem";
+import './GalleryList.css';
+import Paper from '@mui/material/Paper';
+
 
 function GalleryList({ gallery, heartPhoto, deletePhoto }) {
-    return(
-        <>
-        {gallery.map(each => (
-            <GalleryItem 
-                key={each.id}
-                each={each}
-                heartPhoto={heartPhoto}
-                deletePhoto={deletePhoto}
-            />
-        ))}
-        </>
+    return (
+        <div className="photo-container">
+            {gallery.map(each => (
+                <Paper elevation={5}>
+                    <GalleryItem
+                        key={each.id}
+                        each={each}
+                        heartPhoto={heartPhoto}
+                        deletePhoto={deletePhoto}
+                    />
+                </Paper>
+            ))}
+        </div>
     )
 }
 
