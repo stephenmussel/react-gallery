d2b821b886d968c1f76c330023e9eeaaf6de7b13
phaydara <phaydara@gmail.com>
2022-04-30 12:41:34 -0500

    added form for gallery

--- src/components/GalleryForm/GalleryForm.jsx
+++ src/components/GalleryForm/GalleryForm.jsx
@@ -2,6 +2,30 @@ function GalleryForm() {
     return(
         <>
             <h1>Gallery Form</h1>
+            <form>
+                <input 
+                    type="text"
+                    placeholder="url"
+                    style={{marginBottom: 5}}
+                /><br />
+                <input 
+                    type="text"
+                    placeholder="title"
+                    style={{marginBottom: 5}}
+                /><br />
+                <textarea 
+                    type="text"
+                    rows="5"
+                    cols="21"
+                    placeholder="description"
+                    style={{marginBottom: 5}}
+                ></textarea><br />
+                <input 
+                    type="submit"
+                    value="Add"
+                />
+            </form>
+            <br /><br />
         </>
     )
 }
